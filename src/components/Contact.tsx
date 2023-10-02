eeb33c53b8af86fd99c18de1bd6eed5537f3a82f
Lyean <lyean1500@gmail.com>
2023-10-02 22:46:12 +0630

    update

--- src/components/Contact.tsx
+++
@@ -1,9 +0,0 @@
-const Contact: React.FC = () => {
-    return (
-        <div className="contact">
-
-        </div>
-    )
-}
-
-export default Contact
--- src/components/Services.tsx
+++
@@ -1,9 +0,0 @@
-const Services: React.FC = () => {
-    return (
-        <div className="services">
-
-        </div>
-    )
-}
-
-export default Services
