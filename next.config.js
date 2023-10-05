ad43248e79c7ee276ee06ff33ed1982d8fe484ad
Lyean1 <lyean1500@gmail.com>
2023-10-05 11:59:15 +0630

    update

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
-
-module.exports = nextConfig
+module.exports = {
+  images: {
+    domains: [
+      'res.cloudinary.com',
+      'cdn.hashnode.com',
+      'avatars.githubusercontent.com',
+    ],
+  },
+};
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,29 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import data from '@/data';
+import { Head, Html, Main, NextScript } from 'next/document';
 
-export default function Document() {
+const Document = () => {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link href="/logo.svg" rel="icon" />
+        <link
+          href="/rss.xml"
+          rel="alternate"
+          title="RSS"
+          type="application/rss+xml"
+        />
+        <meta content="/og-image.png" property="og:image" />
+        <meta content={data.keywords} name="keywords" />
+        <link href="/manifest.json" rel="manifest" />
+        <link href="/icon.png" rel="apple-touch-icon" />
+        <meta content="#fff" name="theme-color" />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
-}
+  );
+};
+
+export default Document;
