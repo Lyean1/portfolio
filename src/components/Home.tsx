d8a62e65168a4da2dbce4a1d9a58d07966f62da5
Lyean1 <lyean1500@gmail.com>
2023-10-07 23:59:33 +0630

    update

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,3 +3,8 @@ import myPhoto from "./my-photo.jpg";
 
 const Home: React.FC = () => {
     const [name, setName] = useState("");
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setName(event.target.value);
+      };
+    
