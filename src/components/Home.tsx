52aa982b5e89fb1dd71ae085dfe0595fb580c8cf
Lyean1 <lyean1500@gmail.com>
2023-10-08 00:00:54 +0630

    update

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,12 +2,24 @@ import React, { useState } from "react";
 import myPhoto from "./my-photo.jpg";
 
 const Home: React.FC = () => {
-    const [name, setName] = useState("");
+  const [name, setName] = useState("");
 
-    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setName(event.target.value);
-      };
-    
-      return (
-        <div>
-          <h1>Welcome to Home, {name}!</h1>
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setName(event.target.value);
+  };
+
+  return (
+    <div>
+      <h1>Welcome to Home, {name}!</h1>
+      <img src={myPhoto} alt="My photo" />
+      <input
+        type="text"
+        placeholder="Enter your name..."
+        value={name}
+        onChange={handleChange}
+      />
+    </div>
+  );
+};
+
+export default Home;
